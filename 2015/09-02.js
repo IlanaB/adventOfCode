a807214e00dd3782565bd66da555a6f58f734f42
Ilana Beer <ibeer@sapient.com>
2016-11-30 16:02:26 -0500

    solve 9.2 and remove comments

--- 2015/09-02.js
+++ 2015/09-02.js
@@ -1,6 +1,7 @@
 "use strict"
 
 const pattern = "Faerun to Tristram = 65,Faerun to Tambi = 129,Faerun to Norrath = 144,Faerun to Snowdin = 71,Faerun to Straylight = 137,Faerun to AlphaCentauri = 3,Faerun to Arbre = 149,Tristram to Tambi = 63,Tristram to Norrath = 4,Tristram to Snowdin = 105,Tristram to Straylight = 125,Tristram to AlphaCentauri = 55,Tristram to Arbre = 14,Tambi to Norrath = 68,Tambi to Snowdin = 52,Tambi to Straylight = 65,Tambi to AlphaCentauri = 22,Tambi to Arbre = 143,Norrath to Snowdin = 8,Norrath to Straylight = 23,Norrath to AlphaCentauri = 136,Norrath to Arbre = 115,Snowdin to Straylight = 101,Snowdin to AlphaCentauri = 84,Snowdin to Arbre = 96,Straylight to AlphaCentauri = 107,Straylight to Arbre = 14,AlphaCentauri to Arbre = 46";
+// const pattern = "London to Dublin = 464,London to Belfast = 518,Dublin to Belfast = 141";
 
 const mapRoutes = routes => {
     let routeMap = {};
@@ -28,9 +29,8 @@ const nextStep = ( map1, distanceTraveled, visited, start, end ) => {
     let newDistanceTraveled = distanceTraveled + map1[start][end];
     let newVisited = Object.assign({}, visited, {[end]: true});
 
-    if( newDistanceTraveled > shortestDistance ) { return false };
     if( Object.keys(newVisited).length === totalPlaces ) {
-        shortestDistance = newDistanceTraveled;
+        if (newDistanceTraveled > longestDistance) longestDistance = newDistanceTraveled;
         return false;
     }
 
@@ -40,7 +40,7 @@ const nextStep = ( map1, distanceTraveled, visited, start, end ) => {
 
 }
 
-let shortestDistance = 999999999999;
+let longestDistance = 0;
 let map = mapRoutes(pattern.split(","));
 const totalPlaces = Object.keys(map).length;
 
@@ -49,5 +49,5 @@ for( const start of Object.keys(map) ) {
         nextStep(map, 0, {[start]: true}, start, end);
     }
 }
-console.log(shortestDistance);
+console.log(longestDistance);
 
