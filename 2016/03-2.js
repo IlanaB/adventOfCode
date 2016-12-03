f0006860666a9bce1ed693664acd64f2fa130289
Ilana Beer <ilanambeer@gmail.com>
2016-12-03 15:46:34 -0500

    solve for 2016 q03-2

--- 2016/03-2.js
+++ 2016/03-2.js
@@ -1,16 +1,38 @@
 "use strict"
 const fs = require('fs');
 
+const addSides = ( total, side ) => total + side;
+
+const isTriangleValid = sides => {
+    let max = Math.max(...sides);
+    return sides.reduce(addSides, -max) > max;
+}
+
 const getTriangles = sets => {
-    return sets.filter(set => {
-        let sides = set.match(/(\d+)/g);
-        let max = Math.max(...sides);
-        return sides.reduce((a,b)=>(+a + +b), -max) > max;
-    });
+    return sets.filter(isTriangleValid);
+}
+
+const getRowNumbers = row => row.match(/(\d+)/g);
+const getSides = ( grid, row, col ) => [Number(grid[row][col]), Number(grid[row + 1][col]), Number(grid[row + 2][col])]
+
+const getSets = data => {
+    let rows = data.split("\n");
+    let numberRows = rows.map(getRowNumbers);
+    let sets = [];
+    let iterations = (rows.length/3 - 1);
+
+    for( let i = 0; i <= iterations; i++) {
+        let startRow = i * 3;
+        sets.push(getSides(numberRows, startRow, 0));
+        sets.push(getSides(numberRows, startRow, 1));
+        sets.push(getSides(numberRows, startRow, 2));
+    }
+    return sets;
 }
 
 fs.readFile('./03.txt', 'utf8', (err, data) => {
   if (err) throw err;
-  let triangles = getTriangles(data.split("\n"));
+  let sets = getSets(data);
+  let triangles = getTriangles(sets);
   console.log(triangles.length);
 });
