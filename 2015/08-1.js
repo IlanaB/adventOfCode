75be9a13f4e96cdf3fb6b7c4db8f8d022d4df769
Ilana Beer <ilanambeer@gmail.com>
2016-12-07 00:01:37 -0500

    struggling with 2015 q8.1

--- 2015/08-1.js
+++ 2015/08-1.js
@@ -2,18 +2,22 @@
 const fs = require('fs');
 
 const decodeChar = ( a, b ) => {
-    return String.fromCharCode(parseInt(b, 16));
+    let charCode = parseInt(b, 16);
+    return String.fromCharCode(charCode);
 }
 
 const getLiteralString = line => {
     let literal = line
         .replace(/^"(.*)"$/,"$1")
-        .replace(/\\x([a-f0-9]{2})/gi, decodeChar)
-        .replace(/\\\\/g,"\\")
+        .replace(/\\x[0-1][a-f0-9]/gi, "")
+        .replace(/(\\x(20|7f|81|8d|8f|90|9d|a0|ad))/gi, "")
+        .replace(/\\x([a-f2-9][a-f0-9])/gi, decodeChar)
         .replace(/\\"/g,"\"")
+        .replace(/\\\\/g,"\\")
         .replace(/\s/g, "");
 
-    console.log(literal);
+    // console.log(literal);
+    // console.log(literal.length);
 
     return literal;        
 }
