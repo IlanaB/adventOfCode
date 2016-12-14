5272cf314a336fa885db85008fafad72b8f4368d
Ilana Beer <ilanambeer@gmail.com>
2016-12-13 22:25:37 -0500

    solve 2016 7.2

--- 2016/07/07-2.js
+++ 2016/07/07-2.js
@@ -1,16 +1,13 @@
 "use strict"
 const fs = require('fs');
-const sslRegex = /([a-z])(?!\1)([a-z])\1[a-z]*\[[a-z\[\]]*\[[a-z]*\2\1\2/i;
-const sslRegex2 = /\[[a-z]*([a-z])(?!\1)([a-z])\1[a-z\[\]]*[\]][a-z]*\2\1\2/i;
+const sslRegex = /([a-z])(?!\1)([a-z])\1([a-z]*\[[a-z]*\])*[a-z]*\[[a-z]*\2\1\2/i;
+const sslRegex2 = /([a-z])(?!\1)([a-z])\1([a-z]*\][a-z]*\[)*[a-z]*\][a-z]*\2\1\2/i;
 
 const testLine = line => {
-    console.log(line);
-    console.log(sslRegex.test(line) || sslRegex2.test(line));
-    return sslRegex.test(line) || sslRegex2.test(line);
+    const isSSL = sslRegex.test(line) || sslRegex2.test(line);
+    return isSSL;
 }
 
-// 158 too low
-// 289 too high
 fs.readFile('./input-q7.txt', 'utf8', (err, data) => {
   const lines = data.split("\n");
   console.log(lines.filter(testLine).length);
