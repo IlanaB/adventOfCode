13c5d783bfe98f97928cb59f30032824cf714a60
Ilana Beer <ilanambeer@gmail.com>
2016-12-20 13:19:18 -0500

    solved 2016 9.2

--- 2016/09/09-2.js
+++ 2016/09/09-2.js
@@ -1,24 +1,39 @@
 "use strict"
 const fs = require('fs');
+const matchNext = /^([A-Z]*)\(([0-9]+)x([0-9]+)\)(.*)$/;
+const isBasic = /^[A-Z]+$/;
 
 const convertStringToBits = str => {
     return str.replace(/([A-Z]+)/g, (match) => match.length);
 }
 
 const getDecompressedLength = str => {
-    let bitString = convertStringToBits(str);
-    let regex = /^([0-9]*)\(([0-9]+)x([0-9]+)\)(.*)$/;
     let remainingStr = str;
-    let matches = remainingStr.match(regex);
+    let matches = remainingStr.match(matchNext);
     let decompressedLength = 0;
 
     while(matches != null) {
-        decompressedLength += (matches[1].length + Number(matches[2]) * Number(matches[3]));
-        remainingStr = matches[4].slice(matches[2]);
-        matches = remainingStr.match(regex);
+        let length = matches[2];
+        let count = matches[3];
+        let repeater = matches[4].slice(0, length);
+        decompressedLength += (matches[1].length + getChunkLength(length, count, repeater));
+        remainingStr = matches[4].slice(length);
+        matches = remainingStr.match(matchNext);
     }
 
     return (decompressedLength + remainingStr.length);
+}
+
+const getChunkLength = (length, count, str) => {
+    let sum = 0;
+
+    if(isBasic.test(str)) {
+        sum = str.length * count;
+    } else {
+        sum = getDecompressedLength(str) * count
+    }
+
+    return sum;
 }
 
 fs.readFile('./input-q9.txt', 'utf8', (err, data) => {
