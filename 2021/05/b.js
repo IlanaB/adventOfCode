4c6b3fd581236843c6f5fed414356009ab73b3f5
IlanaB <ilanambeer@gmail.com>
2021-12-05 16:38:23 -0500

    day 5 2021

---
+++ 2021/05/b.js
@@ -0,0 +1,55 @@
+const processData = require('../helpers').processData;
+
+const sorter = (a, b) => a[0] - b[0] === 0 ? a[1] - b[1] : a[0] - b[0];
+
+const solver = (data) => {
+  const lines = data.split("\n");
+
+  const ventCoords = lines.map(line => {
+    const [start, end] = line.split(' -> ');
+
+    const sorted = [
+      start.split(',').map(Number),
+      end.split(',').map(Number)
+    ].sort(sorter);
+
+    return sorted;
+  });
+
+  const gridSize = ventCoords.reduce(
+    ([maxX, maxY],
+    [[vent1X, vent1Y], [vent2X, vent2Y]]) => 
+      [Math.max(maxX, vent1X, vent2X), Math.max(maxY, vent1Y, vent2Y)], 
+    [0,0]);
+
+  const emptyGrid = new Array(gridSize[1] + 1)
+    .fill([])
+    .map(() => new Array(gridSize[0] + 1)
+      .fill(0));
+
+  const grid = ventCoords.reduce((vents, [start, end]) => {
+    if(start[0] === end[0]) {
+      for(let i = start[1]; i <= end[1]; i++) {
+        vents[i][start[0]]++;
+      }
+    } else if (start[1] === end[1]) {
+      for(let i = start[0]; i <= end[0]; i++) {
+        vents[start[1]][i]++;
+      }
+    } else {
+      const diff = Math.abs(end[0] - start[0]);
+      const dirX = end[0] > start[0] ? 1 : -1;
+      const dirY = end[1] > start[1] ? 1 : -1;
+
+      for(let i = 0; i <= diff; i++) {
+        vents[start[1] + (i * dirY)][start[0] + (i * dirX)]++;
+      }
+    }
+
+    return vents;
+  }, emptyGrid);
+
+  return grid.flat().filter(num => num > 1).length;
+}
+
+processData("05/input.txt", solver);
